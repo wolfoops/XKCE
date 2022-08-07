31520b089aced99dd6c36b271a0c4662e265d6c5
wolfoops <wolfranster@gmail.com>
2022-08-07 14:08:39 +0800

    Update test.lua

--- test.lua
+++ test.lua
@@ -1,3 +1,8 @@
 -- 123
-print'print test'
+local S = string
+local sub, rep, fms = S.sub, S.rep, S.format
+local select = select
+local function prt(...)return print(sub(fms(rep('\t%s',select('#',...)),...),2))end
+prt('in test:',select('#',...),...)
+prt'print test'
 return 'a test'
