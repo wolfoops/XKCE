86d945b478961817bd3393335a9c14f1673a032b
wolfoops <wolfranster@gmail.com>
2022-08-16 14:09:56 +0800

    uncomment cycleFullCompact

--- v220811/xkce/aa_monolite.lua
+++ v220811/xkce/aa_monolite.lua
@@ -647,7 +647,7 @@ onMemRecPreExecute = mr_execute
 onMemRecPostExecute = mr_execute
 ---------------------------------------------------------------------------
 
-function cycleFullCompact(sender,force) local state = not(compactmenuitem.Caption == 'Compact View Mode'); if force~=nil then state = not force end; compactmenuitem.Caption = state and 'Compact View Mode' or 'Full View Mode'; getMainForm().Splitter1.Visible = state; getMainForm().Panel4.Visible    = state; getMainForm().Panel5.Visible    = state; end; function addCompactMenu() if compactmenualreadyexists then return end; local parent = getMainForm().Menu.Items; compactmenuitem = createMenuItem(parent); parent.add(compactmenuitem); compactmenuitem.Caption = 'Compact View Mode'; compactmenuitem.OnClick = cycleFullCompact; compactmenualreadyexists = 'yes'; end; addCompactMenu(); --cycleFullCompact(nil,true)
+--function cycleFullCompact(sender,force) local state = not(compactmenuitem.Caption == 'Compact View Mode'); if force~=nil then state = not force end; compactmenuitem.Caption = state and 'Compact View Mode' or 'Full View Mode'; getMainForm().Splitter1.Visible = state; getMainForm().Panel4.Visible    = state; getMainForm().Panel5.Visible    = state; end; function addCompactMenu() if compactmenualreadyexists then return end; local parent = getMainForm().Menu.Items; compactmenuitem = createMenuItem(parent); parent.add(compactmenuitem); compactmenuitem.Caption = 'Compact View Mode'; compactmenuitem.OnClick = cycleFullCompact; compactmenualreadyexists = 'yes'; end; addCompactMenu(); --cycleFullCompact(nil,true)
 ---------------------------------------------------------------------------
 
 function isMonoCheck()return (readInteger'mono_domain_get' or readInteger'GameAssembly.dll') end
